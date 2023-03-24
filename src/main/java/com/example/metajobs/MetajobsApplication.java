cf85992bc82c4415cb5ea31da75c17a069e8cccb
didwndbs <didwndbs@naver.com>
2023-03-24 17:43:11 +0900

    Feat: spring boot setting

---
+++ src/main/java/com/example/metajobs/MetajobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.metajobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetajobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetajobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/metajobs/MetajobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metajobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetajobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
