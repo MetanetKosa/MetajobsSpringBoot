c86f26ae0bfcfc7345ac73ed8f4a220962e7cb79
didwndbs <didwndbs@naver.com>
2023-03-27 17:52:55 +0900

    fix: modify resume

--- src/main/java/com/example/metajobs/resume/ResumeController.java
+++ src/main/java/com/example/metajobs/resume/ResumeController.java
@@ -1,6 +1,8 @@
 package com.example.metajobs.resume;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ResumeController {
@@ -9,5 +11,29 @@ public class ResumeController {
 
     public ResumeController(ResumeService service) {
         this.service = service;
+    }
+    //이력서 등록
+    @PostMapping("/users/{mem_no}/resumes")
+    public void insert(@PathVariable Long mem_no, @RequestBody Resume resume){
+        resume.setMem_no(mem_no);
+        service.insertResume(resume);
+    }
+
+    //이력서 목록
+    @GetMapping("/users/{mem_no}/resumes")
+    public List<Resume> list(@PathVariable Long mem_no){
+
+        return service.getResumeList(mem_no);
+    }
+
+    //이력서 상세조회
+    @GetMapping("/users/{id}/resumes/{resume_no}")
+    public Resume get(@PathVariable Long id,@PathVariable Long resume_no){
+        return service.get(resume_no);
+    }
+
+    @DeleteMapping("/users/{id}/resumes/{resume_no}")
+    public void delete(@PathVariable Long id,@PathVariable Long resume_no){
+        service.delete(resume_no);
     }
 }
--- src/main/java/com/example/metajobs/resume/ResumeMapper.java
+++ src/main/java/com/example/metajobs/resume/ResumeMapper.java
@@ -1,6 +1,6 @@
 package com.example.metajobs.resume;
 
-import org.mapstruct.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
